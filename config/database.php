a7316759b505a55be580edcc6f0dfd8c1695e5cb
suxiaoyan <sutootototo2002@163.com>
2018-12-21 15:28:34 +0800

    修改数据库为tp5

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => '120.78.229.228',
     // 数据库名
-    'database'        => 'mydata',
+    'database'        => 'tp5',
     // 用户名
     'username'        => 'root',
     // 密码
